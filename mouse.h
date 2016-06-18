38a3712a97f0232d6e29bc5577db8b0f4c8ddfab
Eirik Jenssen Husebø <ehu009@mail.uit.no>
2016-06-18 23:53:57 +0200

    wrote a function prototype for looking for mouseclicks

--- mouse.h
+++ mouse.h
@@ -5,4 +5,9 @@
 extern int mouseX, mouseY;
 extern unsigned mouseState;
 
-void mouse_update(SDL_Event *event);
+void mouse_update (SDL_Event *event);
+
+/*
+ * returns non-zero if mouse has been clicked, otherwise zero
+ */
+int mouse_checkout (void);
