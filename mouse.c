aaf2336e8f983b76df336ecba114efeaeaedadbd
Eirik Jenssen Husebø <ehu009@mail.uit.no>
2016-06-18 23:22:45 +0200

    started on implementing mouse handling

--- mouse.c
+++ mouse.c
@@ -1,4 +1,50 @@
 #include "mouse.h"
 
+#include <stdio.h>
 
-/* so empty */
+/*
+ *	Some handler prototypes
+ */
+void mouse_handle_move (SDL_Event *e);
+void mouse_handle_down (SDL_Event *e);
+void mouse_handle_up (SDL_Event *e);
+
+
+void mouse_update (SDL_Event *event)
+{
+	switch (event->type)
+	{
+		case SDL_MOUSEBUTTONDOWN:
+		{
+			mouse_handle_down (event);
+			break;
+		}
+		case SDL_MOUSEBUTTONUP:
+		{
+			mouse_handle_up (event);
+			break;
+		}
+		case SDL_MOUSEMOTION:
+		{
+			mouse_handle_move (event);
+			break;
+		}
+	}
+}
+
+
+void mouse_handle_move (SDL_Event *e)
+{
+	printf("It moves\n");
+}
+
+void mouse_handle_down (SDL_Event *e)
+{
+	printf("It presses\n");
+}
+
+void mouse_handle_up (SDL_Event *e)
+{
+	printf("It releases\n");
+}
+
