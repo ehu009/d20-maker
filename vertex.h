310b6e4cfdf1f2da07441fffdc4baaf4086ea48b
eirik <jenssen.eirik@gmail.com>
2017-05-27 13:51:05 +0200

    vertices: added type for 2-point of pointers

--- vertex.h
+++ vertex.h
@@ -1,6 +1,8 @@
 #ifndef   VERTICES_H_
   #define   VERTICES_H_
 
+
+
   struct vertex_2point_integer
   {
     int pts[2];
@@ -11,6 +13,25 @@
     double pts[2];
   };
 
+  struct vertex_2point_pointer
+  {
+    void *pts[2];
+  };
+
+
+
+  typedef struct vertex_2point_integer  vtx2i_t;
+
+  typedef struct vertex_2point_double   vtx2d_t;
+
+  typedef struct vertex_2point_pointer  vtx2p_t;
+
+
+  void get_vtx2i_from_vtx2d (vtx2d_t *src, vtx2i_t *dst);
+
+  vtx2i_t *make_vtx2i_from_vtx2d (vtx2d_t *src);
+
+
 
   struct vertex_5point_integer
   {
@@ -18,18 +39,11 @@
   };
 
 
-  typedef struct vertex_2point_integer  vtx2i_t;
-
-  typedef struct vertex_2point_double   vtx2d_t;
-
 
   typedef struct vertex_5point_integer  vtx5i_t;
 
 
 
-  void get_vtx2i_from_vtx2d (vtx2d_t *src, vtx2i_t *dst);
-
-  vtx2i_t *make_vtx2i_from_vtx2d (vtx2d_t *src);
 
 
 #endif    /*  VERTICES_H_ */
