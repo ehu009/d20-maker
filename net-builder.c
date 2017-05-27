f713c46dbc12b0b21d8c9fdf81348e2d9454228d
eirik <jenssen.eirik@gmail.com>
2017-05-27 12:44:06 +0200

    test code for manipulating first triangle

--- net-builder.c
+++ net-builder.c
@@ -33,8 +33,6 @@ static struct
 } application;
 
 
-
-
 int rotate_root_triangle(void)
 { //  toggle between rotation and resizing of root triangle
   if((mouse_middle() == -1)
@@ -55,15 +53,94 @@ int slide_selector(void)
 }
 
 
+/*
+ *  model of vertices in an icosahedron
+ */
+vtx5_t d20_model[12] =
+{
+ {.pts = {1,2,3,4,5}},
+ {.pts = {0,2,5,6,7}},
+ {.pts = {0,1,7,8,3}},
+ {.pts = {0,2,4,8,9}},
+ {.pts = {0,3,5,9,10}},
+ {.pts = {0,1,4,6,10}},
+ {.pts = {1,5,7,10,11}},
+ {.pts = {1,2,6,8,11}},
+ {.pts = {2,3,7,9,11}},
+ {.pts = {3,4,8,10,11}},
+ {.pts = {4,5,6,10,11}},
+ {.pts = {7,8,3,10,11}}
+};
+
+static struct
+{
+  double x, y;
+  double radius;
+  int rotation;
+  int num_faces;
+
+  vtx2_t *pos[12];
+  vtx2_t *invalid[12];
+  vtx5_t linkage[12];
+} d20;
+
+
+
+void app_start (void)
+{
+  application.rotate_root = 1;
+  application.use_slider = 1;
+  application.status = APP_START;
+
+  // initialize structures
+  int j = 0;
+  for (; j < 12; j ++)
+  {
+    d20.pos[j] = NULL;
+    d20.invalid[j] = NULL;
+    d20.linkage[j] = d20_model[j];
+  }
+
+  d20.num_faces = 0;
+  d20.radius = 30.0;
+  d20.x = 50.0;
+  d20.y = 50.0;
+
+  d20.pos[0] = calloc (1, sizeof(vtx2_t));
+  d20.pos[1] = calloc (1, sizeof(vtx2_t));
+  d20.pos[2] = calloc (1, sizeof(vtx2_t));
+
+}
+
+void app_free (void)
+{
+  //  free initialized structures
+  int j = 0;
+  for (; j < 12; j ++)
+  {
+    if (d20.pos[j] != NULL)
+      free (d20.pos[j]);
+    if (d20.invalid[j] != NULL)
+      free (d20.invalid[j]);
+  }
+
+
+}
+
+
 void app_draw (void)
 {
   SDL_FillRect (canvas, NULL, 0x0ff);
   SDL_BlitSurface (src_image, NULL, canvas, NULL);
 
+  if (!d20.num_faces)
+  {
+    //fill_invert_screen_triangle (d20.pos[0], d20.pos[1], d20.pos[2], canvas);
+    draw_line2 (canvas, d20.pos[0], d20.pos[1], invertPixel, 0);
+    draw_line2 (canvas, d20.pos[1], d20.pos[2], invertPixel, 0);
+    draw_line2 (canvas, d20.pos[2], d20.pos[0], invertPixel, 0);
+  }
 }
-
-
-
 
 void relocate_and_undo (void)
 {
@@ -79,17 +156,21 @@ void relocate_and_undo (void)
 
 
 
-
 void app_usage ()
 {
   int mX, mY;
   mouse_position (&mX, &mY);
 
-  if (1)
+  if (!d20.num_faces)
   { //  root triangle is not pinned
 
-    // set triangle position
-
+    if (mouse_moves())
+    { // set triangle position
+      d20.x = mX;
+      d20.y = mY;
+    }
+    else
+    {
     if (mouse_left() == -1)
     {
       // pin
@@ -100,6 +181,7 @@ void app_usage ()
       if (scroll)
       {
         // increase radius
+        d20.radius += scroll*1.75;
       }
     }
     else
@@ -107,8 +189,28 @@ void app_usage ()
       if (scroll)
       {
         // rotate
+        d20.rotation += scroll;
       }
     }
+    }
+
+    double radi = d20.radius;
+    double r1 = (90 - (30 * d20.rotation)),
+        r2 = (90 - (30 * d20.rotation) - 120),
+        r3 = (90 - (30 * d20.rotation) + 120);
+    r1 *= M_PI/180;
+    r2 *= M_PI/180;
+    r3 *= M_PI/180;
+
+    d20.pos[0]->pts[0] = d20.x + sin(r1) * radi;
+    d20.pos[0]->pts[1] = d20.y + cos(r1) * radi;
+
+    d20.pos[1]->pts[0] = d20.x + sin(r2) * radi;
+    d20.pos[1]->pts[1] = d20.y + cos(r2) * radi;
+
+    d20.pos[2]->pts[0] = d20.x + sin(r3) * radi;
+    d20.pos[2]->pts[1] = d20.y + cos(r3) * radi;
+
   }
   else
   {
@@ -149,17 +251,4 @@ void app_usage ()
 }
 
 
-void app_start (void)
-{
-  application.rotate_root = 1;
-  application.use_slider = 1;
-  application.status = APP_START;
 
-  // initialize structures
-}
-
-void app_free (void)
-{
-  //  free initialized structures
-
-}
