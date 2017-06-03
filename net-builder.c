3810d153bda774af9250c4aadd721a8f259b8fb3
eirik <jenssen.eirik@gmail.com>
2017-06-04 00:56:00 +0200

    test code for triangle filling functionality

--- net-builder.c
+++ net-builder.c
@@ -161,9 +161,150 @@ void app_free (void)
     if (s->invalid != NULL)
       free (s->invalid);
   }
-
 }
 
+
+SDL_Rect *get_bounds_of_triangle (vtx2d_t *a, vtx2d_t *b, vtx2d_t *c)
+{
+  SDL_Rect *ptr = malloc (sizeof (SDL_Rect));
+  if (ptr != NULL)
+  {
+    vtx2d_t lowest = *a, highest = *a;
+
+    if (lowest.pts[0] >= b->pts[0])
+      lowest.pts[0] = b->pts[0];
+    if (lowest.pts[1] >= b->pts[1])
+      lowest.pts[1] = b->pts[1];
+    if (lowest.pts[0] >= c->pts[0])
+      lowest.pts[0] = c->pts[0];
+    if (lowest.pts[1] >= c->pts[1])
+      lowest.pts[1] = c->pts[1];
+
+    if (highest.pts[0] <= b->pts[0])
+      highest.pts[0] = b->pts[0];
+    if (highest.pts[1] <= b->pts[1])
+      highest.pts[1] = b->pts[1];
+    if (highest.pts[0] <= c->pts[0])
+      highest.pts[0] = c->pts[0];
+    if (highest.pts[1] <= c->pts[1])
+      highest.pts[1] = c->pts[1];
+
+    ptr->x = lowest.pts[0];
+    ptr->y = lowest.pts[1];
+    ptr->w = highest.pts[0] + 1.5;
+    ptr->h = highest.pts[1] + 1.5;
+
+    ptr->w -= ptr->x;
+    ptr->h -= ptr->y;
+  }
+  return ptr;
+}
+
+
+
+
+void fill_test (vtx2d_t *a, vtx2d_t *b, vtx2d_t *c)
+{
+  SDL_Rect *rect = get_bounds_of_triangle (a, b, c);
+
+  SDL_Surface *surf = NULL;
+
+
+
+  #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    surf = SDL_CreateRGBSurface (0, rect->w, rect->h, 32, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
+  #else
+    surf = SDL_CreateRGBSurface (0, rect->w, rect->h, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
+  #endif
+
+
+  SDL_SetSurfaceBlendMode(surf,SDL_BLENDMODE_BLEND);
+ // SDL_BlitSurface (canvas, &rect, surf, NULL);
+
+  SDL_Rect clip_rect;
+  SDL_GetClipRect(canvas, &clip_rect);
+  SDL_SetClipRect(canvas, rect);
+
+  vtx2i_t A, B, C;
+  get_vtx2i_from_vtx2d (a, &A);
+  get_vtx2i_from_vtx2d (b, &B);
+  get_vtx2i_from_vtx2d (c, &C);
+  A.pts[0] -= rect->x;
+  B.pts[0] -= rect->x;
+  C.pts[0] -= rect->x;
+  A.pts[1] -= rect->y;
+  B.pts[1] -= rect->y;
+  C.pts[1] -= rect->y;
+
+  uint32_t pixel, markup = SDL_MapRGBA(surf->format, 0xff,0xff,0,0);
+
+  draw_line2 (surf, &A, &B, colourPixel, markup);
+  draw_line2 (surf, &C, &B, colourPixel, markup);
+  draw_line2 (surf, &A, &C, colourPixel, markup);
+
+  vtx2i_t p;
+  int i = 0, j = 0;
+
+  for (i=0; i <= rect->w; i++)
+  {
+    p.pts[0] = i;
+    int upper = -1, lower = -1;
+
+    for (j = 0; j <= rect->h; j++)
+    {
+      p.pts[1] = j;
+      pixel = getPixel (surf, &p);
+      if (pixel != markup)
+        continue;
+      upper = j;
+      break;
+
+    }
+    if (upper != -1)
+    {
+      lower = upper;
+      for (j = rect->h; j >=upper; j --)
+      {
+        p.pts[1] = j;
+        pixel = getPixel (surf, &p);
+        if (pixel != markup)
+          continue;
+        lower = j;
+        break;
+      }
+      p.pts[1] = upper;
+      vtx2i_t p2 = {.pts = {i, lower}};
+      draw_line2 (surf, &p, &p2, invertPixel, 0);
+    }
+  }
+
+  SDL_SetClipRect(canvas, &clip_rect);
+
+
+  SDL_BlitSurface (surf, NULL, canvas, rect);
+
+  SDL_FreeSurface (surf);
+  free(rect);
+}
+/*
+ uint32_t pixel;
+
+  for (i = b_x; i <= surf->w; i++)
+  {
+    int upper = -1, lower = -1;
+
+    for (j = b_y; j <= surf->h; j++)
+    {
+      pixel = getPixel(surf, i, j);
+      if (pixel != markup)
+        continue;
+      upper = j;
+      break;
+
+    }
+    *
+  }
+  */
 void draw_root (void)
 {
   vtx2i_t p1, p2, p3;
@@ -174,6 +315,17 @@ void draw_root (void)
   draw_line2 (canvas, &p1, &p2, invertPixel, 0);
   draw_line2 (canvas, &p2, &p3, invertPixel, 0);
   draw_line2 (canvas, &p3, &p1, invertPixel, 0);
+  /*
+  draw_line(canvas, p1.pts[0],p1.pts[1], p2.pts[0],p2.pts[1], colourPixel, 0x00ff00);
+  draw_line(canvas, p3.pts[0],p3.pts[1], p2.pts[0],p2.pts[1], colourPixel, 0x00ff00);
+  draw_line(canvas, p1.pts[0],p1.pts[1], p3.pts[0],p3.pts[1], colourPixel, 0x00ff00);
+*/
+  vtx2d_t a  = {.pts = {30,30}},
+      b = {.pts = {30, 80}},
+      c = {.pts = {application.mX, application.mY}};
+
+  fill_test(&a, &b, &c);
+
 }
 
 
--- vertex.c
+++ vertex.c
@@ -3,8 +3,8 @@
 
 void get_vtx2i_from_vtx2d (vtx2d_t *src, vtx2i_t *dst)
 {
-  dst->pts[0] = (int) src->pts[0];
-  dst->pts[1] = (int) src->pts[1];
+  dst->pts[0] = (int) src->pts[0] + 0.5;
+  dst->pts[1] = (int) src->pts[1] + 0.5;
 }
 
 vtx2i_t *make_vtx2i_from_vtx2d (vtx2d_t *src)
@@ -12,8 +12,7 @@ vtx2i_t *make_vtx2i_from_vtx2d (vtx2d_t *src)
   vtx2i_t *ptr = calloc (1, sizeof (vtx2i_t));
   if (ptr != NULL)
   {
-    ptr->pts[0] = (int) src->pts[0];
-    ptr->pts[1] = (int) src->pts[1];
+    get_vtx2i_from_vtx2d (src, ptr);
   }
   return ptr;
 }
