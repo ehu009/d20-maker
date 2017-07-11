7510f7b4e9931ff22aac051cdb3f83d134c024e2
eirik <jenssen.eirik@gmail.com>
2017-07-11 12:44:46 +0200

    simple interface for output file name management

--- src/storage.h
+++ src/storage.h
@@ -1,6 +1,14 @@
 #ifndef   STORAGE_H_
   #define   STORAGE_H_
 
+  #include <dirent.h>
 
+  /*
+   *  Manages names for output files
+   */
+
+  #define OUTPUT_DIR_PATH "src/\0"
+
+  void new_output_path (char *buf);
 
 #endif
