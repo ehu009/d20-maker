6c094ae6e9305598fc58d9b280cacbbfd4318a16
eirik <jenssen.eirik@gmail.com>
2017-07-12 07:58:32 +0200

    storage prototypes

--- src/storage.c
+++ src/storage.c
@@ -1,1 +1,30 @@
+#include <dirent.h>
+#include <sys/ftypes.h>
+
 #include "storage.h"
+
+#define OUTPUT_DIR_PATH "src/\0"
+
+
+
+
+
+int name_exists (char *name)
+{
+
+
+}
+
+
+void generate_name (char *buf, SDL_Surface *surface)
+{
+
+
+}
+
+
+void store_subsurface (SDL_Surface *surface, SDL_Rect *rect)
+{
+
+
+}
