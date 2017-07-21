580cfb17c3d079d140b11e42b4d0b08556e2a19b
eirik <jenssen.eirik@gmail.com>
2017-07-21 13:14:33 +0200

    reworked toggler interface

--- src/toggler.h
+++ src/toggler.h
@@ -1,12 +1,12 @@
 #ifndef     TOGGLE_H_
 #define     TOGGLE_H_
 
-#include  <stdlib.h>
-// #include   <stdio.h>
-#include  <string.h>
 
-#include  "SDL2/SDL.h"
-#include  "SDL2/SDL_ttf.h"
+#include  "SDL2/SDL_surface.h"
+
+#include  "mouse_app.h"
+
+extern SDL_Surface *canvas;
 
 
 struct toggle_switch;
@@ -14,12 +14,14 @@ struct toggle_switch;
 typedef struct toggle_switch toggler_t;
 
 
-toggler_t *toggler_create (const char *txt1, const char *txt2, unsigned start);
+toggler_t *toggler_create (const char *txt1, const char *txt2, unsigned *value);
 
 void toggler_free (toggler_t *toggler);
 
 void toggler_setPosition (toggler_t *button, unsigned _x, unsigned _y);
 
-unsigned toggler_update (toggler_t *button, SDL_Surface *screen, unsigned mouseX, unsigned mouseY, unsigned mouseIsDown);
+void toggler_update (toggler_t *button);
+
+void toggler_draw (toggler_t *button);
 
 #endif    /*  TOGGLE_H_ */
