3a349503391fc35e0f043b4d547f616dd9f9d9ed
eirik <jenssen.eirik@gmail.com>
2017-07-21 12:58:26 +0200

    renaming in slider implementation

--- src/slider.h
+++ src/slider.h
@@ -1,30 +1,31 @@
-#ifndef			UI_SLIDER_H_
-#define			UI_SLIDER_H_
+#ifndef     SLIDER_H_
+#define     SLIDER_H_
 
-#include	<stdlib.h>
-#include 	<stdio.h>
-#include	<string.h>
+#include  <stdlib.h>
+#include  <stdio.h>
+#include  <string.h>
 
-#include	"SDL2/SDL.h"
-#include	"SDL2/SDL_ttf.h"
-
-
-struct UI_slide_bar;
-
-typedef struct UI_slide_bar	UI_slider;
-
-
-UI_slider *UI_slider_create (double start, int width, int height);
-
-void 	UI_slider_free (UI_slider *slide);
-
-
-void UI_slider_setPos (UI_slider *slide, int _x, int _y);
-
-int UI_slider_setFont (UI_slider *slide, const char *font, unsigned font_size, SDL_Color font_color);
-
-double UI_slider_update (UI_slider *slide, unsigned mouseX, unsigned mouseY, unsigned mouseIsDown, SDL_Surface *screen);
+#include  "SDL2/SDL.h"
+#include  "SDL2/SDL_ttf.h"
 
 
 
-#endif		/*	UI_SLIDER_H_	*/
+struct slide_bar;
+
+typedef struct slide_bar  slider_t;
+
+
+slider_t *slider_create (double start, int width, int height);
+
+void  slider_free (slider_t *slide);
+
+
+void slider_setPos (slider_t *slide, int _x, int _y);
+
+int slider_setFont (slider_t *slide, const char *font, unsigned font_size, SDL_Color font_color);
+
+double slider_update (slider_t *slide, unsigned mouseX, unsigned mouseY, unsigned mouseIsDown, SDL_Surface *screen);
+
+
+
+#endif    /*  SLIDER_H_  */
