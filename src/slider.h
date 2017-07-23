ce336b1a2ed08e238eb8e09a7fc600f8e7540a16
eirik <jenssen.eirik@gmail.com>
2017-07-23 12:49:01 +0200

    reworked slider interface

--- src/slider.h
+++ src/slider.h
@@ -1,13 +1,11 @@
 #ifndef     SLIDER_H_
 #define     SLIDER_H_
 
-#include  <stdlib.h>
-#include  <stdio.h>
-#include  <string.h>
 
-#include  "SDL2/SDL.h"
-#include  "SDL2/SDL_ttf.h"
+#include  "SDL2/SDL_surface.h"
 
+
+extern SDL_Surface *canvas;
 
 
 struct slide_bar;
@@ -15,16 +13,16 @@ struct slide_bar;
 typedef struct slide_bar  slider_t;
 
 
-slider_t *slider_create (double start, int width, int height);
+slider_t *slider_create (double start, int width, int height, double *value);
 
 void  slider_free (slider_t *slide);
 
 
 void slider_setPos (slider_t *slide, int _x, int _y);
 
-int slider_setFont (slider_t *slide, const char *font, unsigned font_size, SDL_Color font_color);
+void slider_draw (slider_t *slide);
 
-double slider_update (slider_t *slide, unsigned mouseX, unsigned mouseY, unsigned mouseIsDown, SDL_Surface *screen);
+void slider_update (slider_t *slide);
 
 
 
