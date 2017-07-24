c1270cd55a1d277b86423a292186b0341710bc4b
eirik <jenssen.eirik@gmail.com>
2017-07-24 14:58:59 +0200

    rewrote slider implementation to fit new interface

--- src/slider.c
+++ src/slider.c
@@ -1,15 +1,15 @@
+#include  <stdlib.h>
+#include <stdio.h>
+
+#include  "SDL2/SDL_ttf.h"
 #include  "slider.h"
+#include  "mouse_app.h"
 
 
-enum {SLIDESTATUS_OVER = 1, SLIDESTATUS_DOWN = 2};
+enum {SLIDESTATUS_OVER = 1, SLIDESTATUS_DOWN = 2, SLIDESTATUS_UP = 4};
 
-struct mouseTrack
-{
-  unsigned msX, msY;
-  int dragx, dragy;
-};
 
-struct slide_bar
+struct fader_bar
 {
   SDL_Surface *surface;
   SDL_Rect  bar;
@@ -17,41 +17,15 @@ struct slide_bar
   SDL_Color font_color;
   unsigned  posx, posy;
   double    position;
+  double    *value;
   unsigned  status;
-  struct  mouseTrack mouse;
+  unsigned clr1, clr2;
+  unsigned diff;
 };
 
 
 
-slider_t *slider_create (double start, int width, int height)
-{
-  SDL_Surface *surf = SDL_CreateRGBSurface (0, width, height, 32, 0,0,0,0);
-  if (surf == NULL)
-    return NULL;
-  slider_t *slide  = (slider_t *) malloc (sizeof(slider_t));
-  if (slide != NULL)
-  {
-    slide->posx = 0;
-    slide->posy = 0;
-    slide->font_color = (SDL_Color) {0,0,0};
-    slide->position = start;
-    slide->font = NULL;
-    slide->bar    = (SDL_Rect) {.x=0, .y=0, .w=width, .h=height};
-    slide->surface  = surf;
-    slide->status = 0;
-  }
-  return slide;
-}
-
-void slider_setPos (slider_t *slide, int _x, int _y)
-{
-  if (_x >= 0)
-    slide->posx = _x;
-  if (_y >= 0)
-    slide->posy = _y;
-}
-
-int slider_setFont (slider_t *slide, const char *font, unsigned font_size, SDL_Color font_color)
+int fader_setFont (fader_t *slide, const char *font, unsigned font_size, SDL_Color font_color)
 {
   if (slide->font != NULL)
   {
@@ -64,27 +38,69 @@ int slider_setFont (slider_t *slide, const char *font, unsigned font_size, SDL_C
   return 0;
 }
 
-void _slider_draw (slider_t *slide, SDL_Surface *screen, unsigned bar_color, unsigned btn_color)
-{
 
-  SDL_Rect temp_rect;
+
+fader_t *fader_create (double start, int width, int height, double *value)
+{
+  SDL_Surface *surf = SDL_CreateRGBSurface (0, width, height, 32, 0,0,0,0);
+  if (surf == NULL)
+    return NULL;
+  fader_t *slide  = (fader_t *) malloc (sizeof(fader_t));
+  if (slide != NULL)
+  {
+    slide->posx = 0;
+    slide->posy = 0;
+    slide->font_color = (SDL_Color) {0,0,0};
+    slide->position = start;
+    slide->font = NULL;
+    slide->bar    = (SDL_Rect) {.x=0, .y=0, .w=width, .h=height};
+    slide->surface  = surf;
+    slide->status = 0;
+    slide->clr1 = 0xff0000;
+    slide->clr2 = 0x0ff;
+    slide->value = value;
+    fader_setFont(slide, "cour.ttf\0", 12, (SDL_Color) {0,0,0});
+  }
+  return slide;
+}
+
+void fader_setPos (fader_t *slide, int _x, int _y)
+{
+  if (_x >= 0)
+    slide->posx = _x;
+  if (_y >= 0)
+    slide->posy = _y;
+}
+
+
+void get_button_rect (fader_t *slide, SDL_Rect *buf)
+{
   if (slide->bar.w > slide->bar.h)
   {
-    temp_rect = (SDL_Rect) {
-      .x = (0.5 + (slide->bar.w -slide->bar.h) * slide->position / 100),
+    *buf = (SDL_Rect) {
+      .x = (0.5 + ((slide->bar.w -slide->bar.h) * slide->position) / 100),
       .y = slide->bar.y,    .w = slide->bar.h,    .h = slide->bar.h
     };
   }
   else
   {
-    temp_rect = (SDL_Rect) {
-      .y = (0.5 + (slide->bar.h -slide->bar.w) * slide->position / 100),
+    *buf = (SDL_Rect) {
+      .y = (0.5 + ((slide->bar.h -slide->bar.w) * slide->position) / 100),
       .x = slide->bar.x,    .w = slide->bar.w,    .h = slide->bar.w
     };
   }
+}
 
+
+void _fader_draw (fader_t *slide, SDL_Surface *screen, unsigned bar_color, unsigned btn_color)
+{
+  int horizontal = (slide->bar.w > slide->bar.h);
   SDL_FillRect (slide->surface, NULL, bar_color);
-  SDL_FillRect (slide->surface, &temp_rect, btn_color);
+
+  SDL_Rect button_rect;
+  get_button_rect(slide, &button_rect);
+  SDL_FillRect (slide->surface, &button_rect, btn_color);
+
   if (slide->font != NULL)
   {
     char text[] = {0,0,0,0};
@@ -101,7 +117,7 @@ void _slider_draw (slider_t *slide, SDL_Surface *screen, unsigned bar_color, uns
     {
 
       SDL_Rect text_rect;
-      if (slide->bar.w > slide->bar.h)
+      if (horizontal)
       {
         text_rect = (SDL_Rect) {
           .x = (0.5 + (slide->bar.w - slide->bar.h) * slide->position / 100),
@@ -124,31 +140,28 @@ void _slider_draw (slider_t *slide, SDL_Surface *screen, unsigned bar_color, uns
       SDL_FreeSurface (tmp_surf);
     }
   }
-
   slide->bar.x += slide->posx;
   slide->bar.y += slide->posy;
+
   SDL_BlitSurface (slide->surface, NULL, screen, &slide->bar);
+
   slide->bar.x -= slide->posx;
   slide->bar.y -= slide->posy;
+
 }
 
-double slider_update (slider_t *slide, unsigned mouseX, unsigned mouseY, unsigned mouseIsDown, SDL_Surface *screen)
+void fader_update (fader_t *slide)
 {
+  int horizontal = (slide->bar.w > slide->bar.h);
+  int mouseX, mouseY;
+  mouse_position(&mouseX, &mouseY);
+  int mouseIsDown = (mouse_left() == 1),
+      mouseIsUp = (mouse_left() == -1);
+
+
   SDL_Rect temp_rect;
-  if (slide->bar.w > slide->bar.h)
-  {
-    temp_rect = (SDL_Rect) {
-      .x = (0.5 + (slide->bar.w - slide->bar.h) * slide->position / 100),
-      .y = slide->bar.y,    .w = slide->bar.h,    .h = slide->bar.h
-    };
-  }
-  else
-  {
-    temp_rect = (SDL_Rect) {
-      .y = (0.5 + (slide->bar.h - slide->bar.w) * slide->position / 100),
-      .x = slide->bar.x,    .w = slide->bar.w,    .h = slide->bar.w
-    };
-  }
+  get_button_rect (slide, &temp_rect);
+
   temp_rect.x += slide->posx;
   temp_rect.y += slide->posy;
 
@@ -157,18 +170,20 @@ double slider_update (slider_t *slide, unsigned mouseX, unsigned mouseY, unsigne
   {
     if (!(SLIDESTATUS_OVER & slide->status))
       slide->status |= SLIDESTATUS_OVER;
-    if ((SLIDESTATUS_OVER & slide->status))
+    else
     {
       if (mouseIsDown)
       {
         if (!(SLIDESTATUS_DOWN & slide->status))
         {
-          slide->mouse.msX = mouseX;
-          slide->mouse.msY = mouseY;
+          if (horizontal)
+            slide->diff = mouseX - temp_rect.x;
+          else
+            slide->diff = mouseY - temp_rect.y;
           slide->status |= SLIDESTATUS_DOWN;
         }
       }
-      else
+      if (mouseIsUp)
       {
         if (SLIDESTATUS_DOWN & slide->status)
           slide->status ^= SLIDESTATUS_DOWN;
@@ -177,14 +192,11 @@ double slider_update (slider_t *slide, unsigned mouseX, unsigned mouseY, unsigne
   }
   else
   {
-    if (!(SLIDESTATUS_DOWN & slide->status))
-    {
-      if (SLIDESTATUS_OVER & slide->status)
+    if (SLIDESTATUS_OVER & slide->status)
         slide->status ^= SLIDESTATUS_OVER;
-    }
-    else
+    if (SLIDESTATUS_DOWN & slide->status)
     {
-      if (!mouseIsDown)
+      if (mouseIsUp)
         slide->status ^= SLIDESTATUS_DOWN;
     }
 
@@ -193,58 +205,63 @@ double slider_update (slider_t *slide, unsigned mouseX, unsigned mouseY, unsigne
 
   if (SLIDESTATUS_OVER & slide->status)
   {
-    if (SLIDESTATUS_DOWN & slide->status)
-    {
-      slide->mouse.dragx = mouseX - slide->mouse.msX;
-      slide->mouse.dragy = mouseY - slide->mouse.msY;
-
-
-      int *drag;
-      unsigned *track;
-      unsigned b1, b2, interval;
-      if (slide->bar.w > slide->bar.h)
-      {
-        drag = &slide->mouse.dragx;
-        track = &mouseX;
-        b1 = slide->posx;
-        b2 = slide->bar.w;
-        interval = slide->bar.w - slide->bar.h;
-      }
-      else
-      {
-        drag = &slide->mouse.dragy;
-        track = &mouseY;
-        b1 = slide->posy;
-        b2 = slide->bar.h;
-        interval = slide->bar.h - slide->bar.w;
-      }
-      b2 += b1;
-      if (((*drag > 0) && (b1 < *track))
-          ||  ((*drag < 0) && (b2 > *track)))
-      {
-        double t = (*drag)*100;
-        t /= interval;
-        t += slide->position;
-        if (t < 0.0)
-          t = 0.0;
-        if (t > 100.0)
-          t = 100.0;
-        slide->position = t;
-      }
-      slide->mouse.msX = mouseX;
-      slide->mouse.msY = mouseY;
-      _slider_draw (slide, screen, 0xff0000, 0x00ffff);
-    }
-    else
-      _slider_draw (slide, screen, 0xff0000, 0xffffff);
+    slide->clr1 = 0x0ff0000;
+    slide->clr2 = 0x0ffffff;
   }
   else
-    _slider_draw (slide, screen, 0xff0000, 0xff);
-  return slide->position;
+  {
+    slide->clr1 = 0x0ff0000;
+    slide->clr2 = 0x0ff;
+  }
+  if (SLIDESTATUS_DOWN & slide->status)
+  {
+    slide->clr1 = 0x0ff0000;
+    slide->clr2 = 0x0ffff;
+
+    int *tester = &mouseY;
+    unsigned bMin, bMax;
+
+    if (horizontal)
+    {
+      tester = &mouseX;
+      bMin = slide->posx + slide->diff;
+      bMax = slide->bar.w - (slide->bar.h );
+    }
+    else
+    {
+      bMin = slide->posy + slide->diff;
+      bMax = slide->bar.h - (slide->bar.w );
+    }
+    bMax += bMin;
+
+    if (*tester < bMin)
+    {
+      slide->position = 0.0;
+    }
+    else if (*tester > bMax)
+    {
+      slide->position = 100.0;
+    }
+    else
+    {
+      slide->position = *tester;
+      slide->position -= bMin;
+      slide->position *= 100;
+      if (horizontal)
+        slide->position /= (slide->bar.w - slide->bar.h);
+      else
+        slide->position /= (slide->bar.h - slide->bar.w);
+    }
+  }
+  *slide->value = slide->position;
 }
 
+void fader_draw (fader_t *slide)
+{
+  _fader_draw (slide, canvas, slide->clr1, slide->clr2);
+}
 
-void  slider_free (slider_t *slide)
+void  fader_free (fader_t *slide)
 {
   if (slide != NULL)
   {
