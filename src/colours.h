8fc1826c307ac11dc626bad206bebc6588838716
eirik <jenssen.eirik@gmail.com>
2017-07-31 03:17:56 +0200

    changed colours for buttons, slide-bar, background

--- src/colours.h
+++ src/colours.h
@@ -5,25 +5,26 @@
 
   extern unsigned alpha_mask;
 
-  #define   FONT_COLOUR   (SDL_Color) {0,0,0}
+  #define   FONT_COLOUR_WHITE   (SDL_Color) {0xff,0xff,0xff}
+  #define   FONT_COLOUR_BLACK   (SDL_Color) {0,0,0}
 
-  #define   COLOUR_BACKGROUND   0x0ff
+  #define   COLOUR_BACKGROUND   0x0dbdbdb
 
 
   #define   TRIANGLE_COLOUR_PINNED   0xff000000
   #define   TRIANGLE_COLOUR_UNPINNED 0xffff0000
 
 
-  #define   BUTTON_COLOUR_DEFAULT 0xff0000
-  #define   BUTTON_COLOUR_OVER    0xffffff
-  #define   BUTTON_COLOUR_DOWN    0x00ffff
+  #define   BUTTON_COLOUR_DEFAULT 0x303030
+  #define   BUTTON_COLOUR_OVER    0x595959
+  #define   BUTTON_COLOUR_DOWN    0x0ff0000
 
 
-  #define   FADER_COLOUR_BAR      0xff0000
+  #define   FADER_COLOUR_BAR      0xffffff
 
-  #define   FADER_COLOUR_DEFAULT  0x0ff
-  #define   FADER_COLOUR_OVER     0x0ffffff
-  #define   FADER_COLOUR_DOWN     0x0ffff
+  #define   FADER_COLOUR_DEFAULT  0x303030
+  #define   FADER_COLOUR_OVER     0x595959
+  #define   FADER_COLOUR_DOWN     0x0ff0000
 
 
 
