69b2d79f71587d40731d8d2f9a47fdc0ec3c47b3
eirik <jenssen.eirik@gmail.com>
2022-04-14 15:05:21 +0200

    ADT for unordered list

---
+++ src/list.c
@@ -0,0 +1,140 @@
+#include <stdlib.h>
+#ifdef _WIN32
+  #include <malloc.h>
+#endif
+
+#include "list.h"
+
+
+struct list_node;
+typedef struct list_node l_node;
+
+struct list_node
+{
+  l_node *next;
+  void *item;
+};
+
+l_node *make_list_node(void *item)
+{
+  l_node *n = malloc(sizeof(l_node));
+  if (n != NULL)
+  {
+    n->item = item;
+  }
+  return n;
+}
+
+
+
+struct linked_list
+{
+  l_node *head;
+  int size;
+};
+
+list_t *make_list(void)
+{
+  list_t *l = malloc(sizeof(list_t));
+  if (l != NULL)
+  {
+    l->head = NULL;
+    l->size = 0;
+  }
+  return l;
+}
+
+void free_list(list_t *list)
+{
+  if (list != NULL)
+  {  
+    while (list->head != NULL)
+    {
+      l_node *next = list->head->next;
+      free(list->head);
+      list->head = next;
+    }
+  }
+}
+
+int list_insert(list_t *list, void *item)
+{
+  if (list != NULL)
+  {
+    l_node *node = make_list_node(item);
+    if (node != NULL)
+    {
+      node->next = list->head;
+      list->head = node;
+      list->size += 1;
+    }
+  }
+  return 1;
+}
+
+void list_remove(list_t *list, void *item)
+{
+  if (list == NULL)
+  {
+    return;
+  }
+  l_node **p = &list->head;
+  l_node *c = *p;
+  while(c != NULL)
+  {
+    l_node *n = c->next;
+    if (c->item == item)
+    {
+      *p = n;
+    }
+      p = &c->next;
+      c = n;
+      
+      
+      
+    
+  }
+}
+
+
+struct linked_list_iterator
+{
+  list_t *list;
+  l_node *current;
+};
+
+list_i *make_list_iterator(list_t *list)
+{
+  list_i *iter = NULL;
+  if (list != NULL)
+  {
+    iter = malloc(sizeof(list_i));
+    if (iter != NULL)
+    {
+      iter->list = list;
+      iter->current = list->head;
+    }
+  }
+  return iter;
+}
+
+void free_list_iterator(list_i *iterator)
+{
+  free(iterator);
+}
+
+void *list_iterator_next(list_i *iterator)
+{
+  if (iterator != NULL)
+  {
+    if (iterator->current != NULL)
+    {
+      void *item = iterator->current->item;
+      iterator->current = iterator->current->next;
+      return item;
+    }
+  }
+  return NULL;
+}
+
+
---
+++ src/list.h
@@ -0,0 +1,24 @@
+#ifndef   LISTS_H_
+#define   LISTS_H_
+
+
+struct linked_list;
+typedef struct linked_list list_t;
+
+list_t *make_list(void);
+void free_list(list_t *list);
+
+int list_insert(list_t *list, void *item);
+void list_remove(list_t *list, void *item);
+
+
+struct linked_list_iterator;
+typedef struct linked_list_iterator list_i;
+
+list_i *make_list_iterator(list_t *list);
+void free_list_iterator(list_i *iterator);
+
+void *list_iterator_next(list_i *iterator);
+
+
+#endif    /*  LISTS_H_ */
