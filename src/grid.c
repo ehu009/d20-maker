8d2feb8afbcd78d4a288db036bbe192cb0d9539c
puddingen <jenssen.eirik@gmail.com>
2024-10-13 05:00:36 +0200

    calculate size, distance and offset

--- src/grid.c
+++ src/grid.c
@@ -1,11 +1,12 @@
-
+#include <stdlib.h>
 #include "vertex.h"
 
 static struct 
 {
   char rotation;
   double distance;
-  vtx2i_t offset, bounds, size;
+  vtx2i_t bounds, size;
+  vtx2d_t offset;
   vtx2d_t **coords;
 } grid;
 
@@ -18,17 +19,87 @@ void grid_rotate (void)
 
 void calc_distance (vtx2d_t *a, vtx2d_t *b, vtx2d_t *c)
 {
+  double d= 0;
+  if (a->pts[0] == b->pts[0])
+  {
+    d = a->pts[1] - b->pts[1];
+  }
+  if (a->pts[0] == c->pts[0])
+  {
+    d = a->pts[1] - c->pts[1];
+  }
+  if (b->pts[0] == c->pts[0])
+  {
+    d = b->pts[1] - c->pts[1];
+  }
+  if (a->pts[1] == b->pts[1])
+  {
+    d = a->pts[0] - b->pts[0];
+  }
+  if (a->pts[1] == c->pts[1])
+  {
+    d = a->pts[0] - c->pts[0];
+  }
+  if (b->pts[1] == c->pts[1])
+  {
+    d = b->pts[0] - c->pts[0];
+  }
   
+  if (d < 0)
+  {
+    d *= -1;
+  }
+  grid.distance = d;
 }
 
 void calc_offset (vtx2d_t *a, vtx2d_t *b, vtx2d_t *c)
 {
+  vtx2d_t *leftmost = a, *topmost = a;
+  if (leftmost->pts[0] > b->pts[0])
+  {
+    leftmost = b;
+  }
+  if (leftmost->pts[0] > c->pts[0])
+  {
+    leftmost = c;
+  }
 
+  if (topmost->pts[1] > b->pts[1])
+  {
+    topmost = b;
+  }
+  if (topmost->pts[1] > c->pts[1])
+  {
+    topmost = c;
+  }
+  
+  double x = grid.distance, y = x * sqrt(3.0);
+  if (grid.rotation)
+  {
+    y = grid.distance;
+    x = y * sqrt(3.0);
+  }
+
+  double *ptr =  &grid.offset.pts[0];
+  *ptr = leftmost->pts[0];
+  while (*ptr >= 0.0)
+  {
+    *ptr -= x;
+  }
+  *ptr += x;
+  ptr =  &grid.offset.pts[1];
+  *ptr = topmost->pts[1];
+  while (*ptr >= 0.0)
+  {
+    *ptr -= y;
+  }
+  *ptr += y;
 }
 
-void calc_size (vtx2d_t *a, vtx2d_t *b, vtx2d_t *c)
+void calc_size (void)
 {
-
+  grid.size.pts[0] = (grid.bounds.pts[0] - grid.offset.pts[0]) / grid.distance;
+  grid.size.pts[1] = (grid.bounds.pts[1] - grid.offset.pts[1]) / grid.distance;
 }
 
 void free_coordinates (vtx2d_t **coords)
@@ -84,12 +155,12 @@ vtx2d_t **init_coordinates ()
 
 unsigned int get_row (vtx2d_t * coord)
 {
-
+  
 }
 
 unsigned int get_col (vtx2d_t * coord)
 {
-  
+
 }
 
 void populate_coordinates (vtx2d_t *a, vtx2d_t *b, vtx2d_t *c)
