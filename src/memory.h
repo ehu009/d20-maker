f5dfa595d16865b3497ef53213635377b393a503
puddingen <jenssen.eirik@gmail.com>
2024-10-14 02:51:47 +0200

    changed signature for 'free' function

--- src/memory.h
+++ src/memory.h
@@ -12,17 +12,15 @@
   
   void init_memory(void);
   void free_memory(void);
+  void memory_report(void);
 
   void *_malloc(size_t size, const char *file);
   void *_calloc(size_t num, size_t size, const char *file);
-
-  void _free(void *ptr);
+  void _free(void *ptr, const char *file, int line);
 
   #define MALLOC(x) _malloc(x, __FILE__);
   #define CALLOC(y, x) _calloc(y, x, __FILE__);
-  #define FREE(x) _free(x);
-
-  void memory_report(void);
+  #define FREE(x) _free(x, __FILE__, __LINE__);
 
 
 #endif
