ed2017d546df7ac3ad1bfe9ee2f28788a211de5c
puddingen <jenssen.eirik@gmail.com>
2024-10-14 04:53:22 +0200

    better readability

--- src/list.c
+++ src/list.c
@@ -18,13 +18,13 @@ struct list_node
 l_node *make_list_node(void *item)
 {
   l_node *n = malloc(sizeof(l_node));
-  if (n != NULL)
+  if (n == NULL)
   {
-    n->item = item;
+    return n;
   }
+  n->item = item;
   return n;
 }
-
 
 
 struct linked_list
@@ -36,35 +36,38 @@ struct linked_list
 list_t *make_list(void)
 {
   list_t *l = malloc(sizeof(list_t));
-  if (l != NULL)
+  if (l == NULL)
   {
-    l->head = NULL;
-    l->size = 0;
+    return l;
   }
+  l->head = NULL;
+  l->size = 0;
   return l;
 }
 
 void free_list(list_t *list)
 {
-  if (list != NULL)
-  {  
-    list_empty(list);
-    free(list);
+  if (list == NULL)
+  {
+    return;
   }
+  list_empty(list);
+  free(list);
 }
 
 void list_empty(list_t *list)
 {
-  if (list != NULL)
+  if (list == NULL)
   {
-    while (list->head != NULL)
-    {
-      l_node *next = list->head->next;
-      free(list->head);
-      list->head = next;
-    }
-    list->size = 0;
+    return;
   }
+  while (list->head != NULL)
+  {
+    l_node *next = list->head->next;
+    free(list->head);
+    list->head = next;
+  }
+  list->size = 0;
 }
 
 
@@ -79,17 +82,18 @@ int list_size(list_t *list)
 
 int list_insert(list_t *list, void *item)
 {
-  if (list != NULL)
+  if (list == NULL)
   {
-    l_node *node = make_list_node(item);
-    if (node != NULL)
-    {
-      node->next = list->head;
-      list->head = node;
-      list->size += 1;
-    }
+    return -1;
   }
-  return 1;
+  l_node *node = make_list_node(item);
+  if (node != NULL)
+  {
+    node->next = list->head;
+    list->head = node;
+    list->size += 1;
+  }
+  return 0;
 }
 
 void list_remove(list_t *list, void *item)
@@ -121,6 +125,10 @@ void list_remove(list_t *list, void *item)
 
 void *list_pop(list_t *list)
 {
+  if (list == NULL)
+  {
+    return NULL;
+  }
   l_node *node = list->head;
   if (node == NULL)
   {
@@ -143,15 +151,18 @@ struct linked_list_iterator
 list_i *make_list_iterator(list_t *list)
 {
   list_i *iter = NULL;
-  if (list != NULL)
+  if (list == NULL)
   {
-    iter = malloc(sizeof(list_i));
-    if (iter != NULL)
-    {
-      iter->list = list;
-      iter->current = list->head;
-    }
+    return iter;
   }
+  
+  iter = malloc(sizeof(list_i));
+  if (iter == NULL)
+  {
+    return iter;
+  }
+  iter->list = list;
+  iter->current = list->head;
   return iter;
 }
 
@@ -162,15 +173,17 @@ void free_list_iterator(list_i *iterator)
 
 void *list_iterator_next(list_i *iterator)
 {
-  if (iterator != NULL)
+  if (iterator == NULL)
   {
-    if (iterator->current != NULL)
-    {
-      void *item = iterator->current->item;
-      iterator->current = iterator->current->next;
-      return item;
-    }
+    return NULL;
   }
-  return NULL;
+  l_node *current = iterator->current;
+  if (current == NULL)
+  {
+    return NULL;
+  }
+  void *item = current->item;
+  iterator->current = current->next;
+  return item;
 }
 
